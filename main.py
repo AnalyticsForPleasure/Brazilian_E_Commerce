80e8b4ffce3fa095d13c2776bd772ee168cb96bc
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-11-01 14:57:07 +0200

    update

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import numpy as np
 import os
+from colorama import Style, Fore
 
 if __name__ == '__main__':
+    pd.set_option('display.max_rows', 500)  # To see all rows
+    pd.set_option('display.max_columns', 500)  # To see all columns
+    pd.set_option('display.width', 1000)
 
     res = os.listdir('data')
 
     for file in res:
         data = pd.read_csv(f'data/{file}')
-        print(data)
+        print(Fore.GREEN + file + "#" * 50 + Style.RESET_ALL)
+        print(data.head())
+        print(Fore.GREEN + "#" * 50 + Style.RESET_ALL)
