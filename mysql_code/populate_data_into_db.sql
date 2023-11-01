da4fa9e1639ff604ad39f28d10028543d266d78d
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-11-01 15:14:12 +0200

    update

--- mysql_code/populate_data_into_db.sql
+++ mysql_code/populate_data_into_db.sql
@@ -1,3 +1,10 @@
+# In case you're using mysql workbench add this variable to the connection:
+# This restriction can be removed from MySQL Workbench 8.0 in the following way. 
+# Edit the connection, on the Connection tab,
+# go to the 'Advanced' sub-tab, 
+# and in the 'Others:' box add the line 'OPT_LOCAL_INFILE=1'.
+
+
 show variables like "secure_file_priv";
 show variables like "local_infile";
 
@@ -14,11 +21,11 @@ create table product_category_name_translation
 
 -- drop table playground_table;
 
--- select * from playground_table;
-
 LOAD DATA LOCAL INFILE '/home/shay_diy/PycharmProjects/Brazilian_E_Commerce/data/product_category_name_translation.csv'
 INTO TABLE product_category_name_translation
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 (product_category_name,product_category_name_english);
 # SET id = NULL;
+
+select * from product_category_name_translation;
